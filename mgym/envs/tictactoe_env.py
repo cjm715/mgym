b4002fe3bc2b8c0c0f4668bdc3b061e33c8daf7a
cjm715 <cjmiles2006@gmail.com>
2018-07-27 19:41:34 -0400

    basic version of tictactoe.. functional but needs some work

--- mgym/envs/tictactoe_env.py
+++ mgym/envs/tictactoe_env.py
@@ -4,13 +4,41 @@ from gym.utils import seeding
 import numpy as np
 import gym.spaces
 
+O = -1
 EMPTY = 0
 X = 1
-O = 2
 
-LOOK_UP_SYM = {0: ' ',
-               1: 'X',
-               2: 'O'}
+LOOK_UP_SYM = {-1: 'O',
+               0: ' ',
+               1: 'X'
+               }
+
+MASK_LIST = [
+    np.array([[1, 0, 0],
+              [0, 1, 0],
+              [0, 0, 1]]),
+    np.array([[0, 0, 1],
+              [0, 1, 0],
+              [1, 0, 0]]),
+    np.array([[1, 1, 1],
+              [0, 0, 0],
+              [0, 0, 0]]),
+    np.array([[0, 0, 0],
+              [1, 1, 1],
+              [0, 0, 0]]),
+    np.array([[0, 0, 0],
+              [0, 0, 0],
+              [1, 1, 1]]),
+    np.array([[1, 0, 0],
+              [1, 0, 0],
+              [1, 0, 0]]),
+    np.array([[0, 1, 0],
+              [0, 1, 0],
+              [0, 1, 0]]),
+    np.array([[0, 0, 1],
+              [0, 0, 1],
+              [0, 0, 1]]),
+]
 
 
 class TicTacToeEnv(gym.Env):
@@ -27,7 +55,8 @@ class TicTacToeEnv(gym.Env):
         self.action_space = []
         self.observation_space = []
         self.grid = None
-        self.whos_up_list = []
+        self.active_agent = None
+        self.agent_sym = [X, O]
 
         for i in range(self.n):
             ind_action_space = spaces.Discrete(self.nA)
@@ -35,26 +64,29 @@ class TicTacToeEnv(gym.Env):
             ind_observation_space = spaces.Discrete(self.nS)
             self.observation_space.append(ind_observation_space)
 
-        self.ordering = 'cyclic'
+    def step(self, action_list):
 
-    def step(self, action):
-
-        observation_list = [None for _ in range(self.n)]
-        reward_list = [None for _ in range(self.n)]
-        done_list = [False for _ in range(self.n)]
-        info_list = [None for _ in range(self.n)]
-
-        observation_list[self.whos_up] = self._observe_grid()
+        self._update_grid(action_list)
 
         if self._game_over():
-            reward_list[self.whos_up] = 0
+            observation = self._observe_grid()
+            observation_list = [observation for _ in range(self.n)]
+            reward_list = [0 for _ in range(self.n)]
+            reward_list[self.active_agent] = 1
             done_list = [True for _ in range(self.n)]
+        else:
+            observation = self._observe_grid()
+            observation_list = [observation for _ in range(self.n)]
+            reward_list = [0 for _ in range(self.n)]
+            done_list = [False for _ in range(self.n)]
 
-        return observation_list, reward_list, done_list, info_list
+        self.active_agent = (self.active_agent + 1) % 2
+        return observation_list, reward_list, done_list, self.active_agent
 
     def reset(self):
         self.grid = np.zeros(self.shape)
-        return self._observe_grid()
+        self.active_agent = 0
+        return self._observe_grid(), self.active_agent
 
     def render(self, mode='human', close=False):
         for i in range(3):
@@ -70,6 +102,30 @@ class TicTacToeEnv(gym.Env):
             for j in range(3):
                 s = self.grid[i, j] + 3 * s
         return s
+
+    def _action_to_grid_indices(self, action):
+        i = int(action // 3)
+        j = int(action % 3)
+        return i, j
+
+    def _update_grid(self, action_list):
+
+        for i, action in enumerate(action_list):
+            if not (action == None):
+                j, k = self._action_to_grid_indices(action)
+                if self.grid[j, k] == EMPTY:
+                    self.grid[j, k] = self.agent_sym[i]
+
+    def _game_over(self):
+        # check for 3 in a row
+        for mask in MASK_LIST:
+            if np.sum(self.grid * mask) == 3:
+                print('X WON!')
+                return True
+            elif np.sum(self.grid * mask) == -3:
+                print('O WON!')
+                return True
+        return False
 
 
 class TicTacToeAgent(object):

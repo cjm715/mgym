7d73e04fbb15488ec0d29876a200c1fd45df514e
cjm715 <cjmiles2006@gmail.com>
2018-07-29 17:27:30 -0400

    added changes to rockpaperscissors

--- mgym/envs/rockpaperscissors_env.py
+++ mgym/envs/rockpaperscissors_env.py
@@ -2,20 +2,82 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
-import gym.spaces
 import mgym
+
+ROCK = 0
+PAPER = 1
+SCISSORS = 2
+
+WORD_FOR_ACTION = {0: 'ROCK',
+                           1: 'PAPER',
+                           2: 'SCISSORS'}
 
 
 class RockPaperScissorsEnv(mgym.MEnv):
 
     def __init__(self):
-        pass
+
+        self.n = 2  # number of players
+        self.nA = 3  # number of actions per player(rock, paper, or scissors)
+        # There are no states! It is a single-shot game. To fit env clas we must
+        # specify an observation space anyway. Thus we will say there is one state and always
+        # one state.
+        self.nS = 1
+        self.record_outcome = None
+        # Joint action space across all players
+        self.action_space = np.Tuple(
+            spaces.Discrete(self.nA), spaces.Discrete(self.nA))
+        self.observation_space = spaces.Discrete(self.nS)
 
     def reset(self):
-        pass
+        self.record_outcome = None
+        return 0
 
-    def step(self):
-        pass
+    def step(self, action):
+        obs = 0
+        rewards = self._get_rewards(action)
+        done = True
+        if rewards[0] > rewards[1]:
+            who_won = 0
+        elif rewards[1] > rewards[0]:
+            who_won = 1
+        else:
+            who_won = None
+        self.record_outcome = (who_won, action)
+        return obs, rewards, done, {}
 
     def render(self):
-        pass
+        who_won, action = self.record_outcome
+        if who_won == None:
+            message = 'Game has not started.')
+        else:
+            if who_won == None:
+                message='Game is Tied.' + 'Player 1 played ' + WORD_FOR_ACTION[action[0]] '.'
+                + 'Player 2 played' + WORD_FOR_ACTION[action[1]].
+            elif who_won=0:
+                message='Player 1 wins!' + 'Player 1 played ' + WORD_FOR_ACTION[action[0]] '.'
+                + 'Player 2 played' + WORD_FOR_ACTION[action[1]].
+            elif who_won=1:
+                message='Player 2 wins!' + 'Player 1 played ' + WORD_FOR_ACTION[action[0]] '.'
+                + 'Player 2 played' + WORD_FOR_ACTION[action[1]].
+        print(message)
+
+    def _get_rewards(self, action):
+        # Cases where player 1 wins
+        if action[0] == ROCK and action[1] == SCISSORS:
+            return (1, -1)
+        if action[0] == SCISSORS and action[1] == PAPER:
+            return (1, -1)
+        if action[0] == PAPER and action[1] == ROCK:
+            return (1, -1)
+
+        # Cases where player 2 wins
+        if action[1] == ROCK and action[0] == SCISSORS:
+            return (-1, 1)
+        if action[1] == SCISSORS and action[0] == PAPER:
+            return (-1, 1)
+        if action[1] == PAPER and action[0] == ROCK:
+            return (-1, 1)
+
+        # otherwise
+        return (0, 0)

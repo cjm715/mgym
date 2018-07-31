5cf9926db2888b0ea2bcc9e817f50cb6b55cc3fa
cjm715 <cjmiles2006@gmail.com>
2018-07-31 17:08:37 -0400

    added battleofthesexes env file

---
+++ mgym/envs/battleofthesexes_env.py
@@ -0,0 +1,21 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+import numpy as np
+import mgym
+from mgym.envs import repeatedgame
+
+
+class BattleOfTheSexesEnv(repeatedgame.RepeatedTwoPlayerGame):
+    '''
+    Repeated Battle Of The Sexes
+    '''
+
+    def __init__(self):
+        WORD_FOR_ACTION = {0: 'OPERA',
+                           1: 'FOOTBALL'}
+        U = [[(3, 2), (0, 0)],
+             [(0, 2), (2, 3)]])
+
+        super(RockPaperScissorsEnv, self).__init__(
+            U, WORD_FOR_ACTION = WORD_FOR_ACTION)

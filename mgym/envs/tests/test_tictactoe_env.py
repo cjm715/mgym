b4c192e56f991f7eaf7ac3ecb7cefdd500dd42cf
cjm715 <cjmiles2006@gmail.com>
2018-07-31 17:52:27 -0400

    mgym/envs/tests/test_game_theory_envs.py

--- mgym/envs/tests/test_tictactoe_env.py
+++ mgym/envs/tests/test_tictactoe_env.py
@@ -1,7 +1,18 @@
 from mgym.envs.tictactoe_env import TicTacToeEnv
 import numpy as np
+import random
+import pytest
 
 
-def test_tictactoe_env_reset():
-    env = TicTacToeEnv()
-    assert np.allclose(np.zeros(env.shape), env.reset())
+def test_that_tictactoe_run_without_error(env_name):
+    env = gym.make('TicTacToe-v0')
+    fullobs = env.reset()
+    while True:
+        print('Player O ') if fullobs[0] else print('Player X')
+        a = random.choice(env.get_available_actions())
+        fullobs, rewards, done, _ = env.step(a)
+        env.render()
+        if done:
+            break
+
+    assert True

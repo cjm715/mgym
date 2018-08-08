d09264da8af5fe0fc4bdd0117aa33eab8b95c83d
cjm715 <cjmiles2006@gmail.com>
2018-08-08 02:26:43 -0400

    setup basic skeleton for traveling checkers env

--- mgym/envs/travelingcheckers_env.py
+++ mgym/envs/travelingcheckers_env.py
@@ -1,7 +1,28 @@
-import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
-import numpy as np
+"""Environment."""
 import mgym
-import copy
 
+
+class TravelingCheckersEnv(mgym.MEnv):
+    """A modified game introduced by the Santa Fe Institute.
+
+    origin:https://www.complexityexplorer.org/challenges/1-launch-of-the-complexity-challenges/submissions
+    description:
+    observations:
+    actions:
+    rewards:
+    """
+
+    def __init__(self):
+        pass
+
+    def reset(self):
+        """Reset environment."""
+        pass
+
+    def step(self, action):
+        """Step environment with action."""
+        pass
+
+    def render(self, action):
+        """Render environment."""
+        pass
